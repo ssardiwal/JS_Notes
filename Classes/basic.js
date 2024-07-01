fd6ee54bf24244373b2635e22f5d722ae5644ec8
ssardiwal <sagarsardiwal1@gmail.com>
2024-07-02 01:26:42 +0530

    Classs note added

---
+++ Classes/basic.js
@@ -0,0 +1,21 @@
+//------------------------------------------------------------------------------------------------------------------------------------------------------
+// Classes are referred as Constructor function
+// new keyword automatically calls the class contructor
+// in case of extends, extends properties must be passed in super
+// this refers to the class scope because this always refers to the object in which it is called
+
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, breed) {
+    super(name); // super calls the parent class constructor so it gets 'name'
+    // and 'name' must be passed while calling using new keyword
+    this.breed = breed;
+  }
+}
+
+let dog1 = new Dog("Buddy", "Golden Retriever");
